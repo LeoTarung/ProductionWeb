fae715bf205af0590c5057f46ba47a9f98b90299
Leonardo Tarung <leotarung@gmail.com>
2022-11-28 13:36:49 +0700

    Memperbesar ukuran footer dna fixed navbar

--- resources/views/partial/footerLHP.blade.php
+++ resources/views/partial/footerLHP.blade.php
@@ -1,6 +1,5 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('/css/boxicons/css/boxicons.css') }}">
 <footer class="mt-auto text-center text-light footerLHP">
-
     <i class='bx bxs-copyright'></i>
     Copyright: P.E. Digitalization
 
