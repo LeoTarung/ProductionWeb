0e7f8364c3d7058a731dd5e8beb30eeed966927a
Leonardo Tarung <leotarung@gmail.com>
2023-01-16 18:52:04 +0700

    edit navbar dan final inspection

--- resources/views/lhp/final.blade.php
+++ resources/views/lhp/final.blade.php
@@ -6,9 +6,53 @@
 
     <div class="container-fluid">
         <div class="row">
+            <div class="col-3 mt-2">
+                <div class="card card-left1 border-dark border border-2 shadow">
+                    <div class="card-left1-inside text-center ">
+                        <div class="font-cl1 fs-4 fw-bold mt-4">SUB-ASSY OIL LG</div>
+                    </div>
+                </div>
+                <div class="card card-left2 mt-2  border-dark border border-2 shadow">
+                    <div class="row text-center">
+                        <div class="col-6">
+                            <div class="font-cl2 fs-4 fw-bold mt-1 ms-4">Total</div>
+                            <div class="font-cl2 fs-4 fw-bold ms-4">Check</div>
+                        </div>
+
+                        <div class="col-6">
+                            <div class="card-left2-inside"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card card-left3 mt-2  border-dark border border-2 shadow">
+                    <div class="row text-center">
+                        <div class="col-6">
+                            <div class="font-cl3 fs-4 fw-bold mt-1 ms-4">Total</div>
+                            <div class="font-cl3 fs-4 fw-bold  ms-4">NG</div>
+                        </div>
+
+                        <div class="col-6">
+                            <div class="card-left3-inside"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card card-left4 mt-2  border-dark border border-2 shadow">
+                    <div class="row text-center">
+                        <div class="col-6">
+                            <div class="font-cl4 fs-4 fw-bold mt-4 ms-4">Total</div>
+                            <div class="font-cl4 fs-4 fw-bold  ms-4">OK</div>
+                        </div>
+
+                        <div class="col-6">
+                            <div class="card-left4-inside"></div>
+                            <div class="card-left4-inside bg-warning"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="col-9">
-                <div class="card">
-                    <div class="row">
+                <div class="card main-card ">
+                    <div class="row row-card-i mt-3 ">
                         <div class="col-3 mt-2">
                             <div class="card-inside shadow-lg border border-2 border border-dark align-items-center">
                                 <div class="card-inside1 float-start">
@@ -39,9 +83,18 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-3 mt-2">4</div>
+                        <div class="col-3 mt-2">
+                            <div class="card-inside shadow-lg border border-2 border border-dark align-items-center">
+                                <div class="card-inside1 float-start">
+                                    <div class="font-ci1">0</div>
+                                </div>
+                                <div class="card-inside2 float-end">
+                                    <div class="font-ci2">GOMPAL</div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="row">
+                    <div class="row row-card-i">
                         <div class="col-3 mt-2">
                             <div class="card-inside shadow-lg border border-2 border border-dark align-items-center">
                                 <div class="card-inside1 float-start">
@@ -72,9 +125,18 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-3 mt-2">4</div>
+                        <div class="col-3 mt-2">
+                            <div class="card-inside shadow-lg border border-2 border border-dark align-items-center">
+                                <div class="card-inside1 float-start">
+                                    <div class="font-ci1">0</div>
+                                </div>
+                                <div class="card-inside2 float-end">
+                                    <div class="font-ci2">GOMPAL</div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="row">
+                    <div class="row row-card-i">
                         <div class="col-3 mt-2">
                             <div class="card-inside shadow-lg border border-2 border border-dark align-items-center">
                                 <div class="card-inside1 float-start">
@@ -95,10 +157,28 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-3 mt-2">3</div>
-                        <div class="col-3 mt-2">4</div>
+                        <div class="col-3 mt-2">
+                            <div class="card-inside shadow-lg border border-2 border border-dark align-items-center">
+                                <div class="card-inside1 float-start">
+                                    <div class="font-ci1">0</div>
+                                </div>
+                                <div class="card-inside2 float-end">
+                                    <div class="font-ci2">DEKOK</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-3 mt-2">
+                            <div class="card-inside shadow-lg border border-2 border border-dark align-items-center">
+                                <div class="card-inside1 float-start">
+                                    <div class="font-ci1">0</div>
+                                </div>
+                                <div class="card-inside2 float-end">
+                                    <div class="font-ci2">GOMPAL</div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="row">
+                    <div class="row row-card-i">
                         <div class="col-3 mt-2">
                             <div class="card-inside shadow-lg border border-2 border border-dark align-items-center">
                                 <div class="card-inside1 float-start">
@@ -119,10 +199,28 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-3 mt-2">3</div>
-                        <div class="col-3 mt-2">4</div>
+                        <div class="col-3 mt-2">
+                            <div class="card-inside shadow-lg border border-2 border border-dark align-items-center">
+                                <div class="card-inside1 float-start">
+                                    <div class="font-ci1">0</div>
+                                </div>
+                                <div class="card-inside2 float-end">
+                                    <div class="font-ci2">DEKOK</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-3 mt-2">
+                            <div class="card-inside shadow-lg border border-2 border border-dark align-items-center">
+                                <div class="card-inside1 float-start">
+                                    <div class="font-ci1">0</div>
+                                </div>
+                                <div class="card-inside2 float-end">
+                                    <div class="font-ci2">GOMPAL</div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="row">
+                    <div class="row row-card-i">
                         <div class="col-3 mt-2">
                             <div class="card-inside shadow-lg border border-2 border border-dark align-items-center">
                                 <div class="card-inside1 float-start">
@@ -143,10 +241,28 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-3 mt-2">3</div>
-                        <div class="col-3 mt-2">4</div>
+                        <div class="col-3 mt-2">
+                            <div class="card-inside shadow-lg border border-2 border border-dark align-items-center">
+                                <div class="card-inside1 float-start">
+                                    <div class="font-ci1">0</div>
+                                </div>
+                                <div class="card-inside2 float-end">
+                                    <div class="font-ci2">DEKOK</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-3 mt-2">
+                            <div class="card-inside shadow-lg border border-2 border border-dark align-items-center">
+                                <div class="card-inside1 float-start">
+                                    <div class="font-ci1">0</div>
+                                </div>
+                                <div class="card-inside2 float-end">
+                                    <div class="font-ci2">GOMPAL</div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="row">
+                    <div class="row row-card-i">
                         <div class="col-3 mt-2">
                             <div class="card-inside shadow-lg border border-2 border border-dark align-items-center">
                                 <div class="card-inside1 float-start">
@@ -167,10 +283,28 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-3 mt-2">3</div>
-                        <div class="col-3 mt-2">4</div>
+                        <div class="col-3 mt-2">
+                            <div class="card-inside shadow-lg border border-2 border border-dark align-items-center">
+                                <div class="card-inside1 float-start">
+                                    <div class="font-ci1">0</div>
+                                </div>
+                                <div class="card-inside2 float-end">
+                                    <div class="font-ci2">DEKOK</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-3 mt-2">
+                            <div class="card-inside shadow-lg border border-2 border border-dark align-items-center">
+                                <div class="card-inside1 float-start">
+                                    <div class="font-ci1">0</div>
+                                </div>
+                                <div class="card-inside2 float-end">
+                                    <div class="font-ci2">GOMPAL</div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="row">
+                    <div class="row row-card-i">
                         <div class="col-3 mt-2">
                             <div class="card-inside shadow-lg border border-2 border border-dark align-items-center">
                                 <div class="card-inside1 float-start">
@@ -191,10 +325,28 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-3 mt-2">3</div>
-                        <div class="col-3 mt-2">4</div>
+                        <div class="col-3 mt-2">
+                            <div class="card-inside shadow-lg border border-2 border border-dark align-items-center">
+                                <div class="card-inside1 float-start">
+                                    <div class="font-ci1">0</div>
+                                </div>
+                                <div class="card-inside2 float-end">
+                                    <div class="font-ci2">DEKOK</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-3 mt-2">
+                            <div class="card-inside shadow-lg border border-2 border border-dark align-items-center">
+                                <div class="card-inside1 float-start">
+                                    <div class="font-ci1">0</div>
+                                </div>
+                                <div class="card-inside2 float-end">
+                                    <div class="font-ci2">GOMPAL</div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="row">
+                    <div class="row row-card-i">
                         <div class="col-3 mt-2">
                             <div class="card-inside shadow-lg border border-2 border border-dark align-items-center">
                                 <div class="card-inside1 float-start">
@@ -215,10 +367,28 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-3 mt-2">3</div>
-                        <div class="col-3 mt-2">4</div>
+                        <div class="col-3 mt-2">
+                            <div class="card-inside shadow-lg border border-2 border border-dark align-items-center">
+                                <div class="card-inside1 float-start">
+                                    <div class="font-ci1">0</div>
+                                </div>
+                                <div class="card-inside2 float-end">
+                                    <div class="font-ci2">DEKOK</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-3 mt-2">
+                            <div class="card-inside shadow-lg border border-2 border border-dark align-items-center">
+                                <div class="card-inside1 float-start">
+                                    <div class="font-ci1">0</div>
+                                </div>
+                                <div class="card-inside2 float-end">
+                                    <div class="font-ci2">GOMPAL</div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="row">
+                    <div class="row row-card-i mb-3">
                         <div class="col-3 mt-2">
                             <div class="card-inside shadow-lg border border-2 border border-dark align-items-center">
                                 <div class="card-inside1 float-start">
@@ -239,8 +409,26 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-3 mt-2">3</div>
-                        <div class="col-3 mt-2">4</div>
+                        <div class="col-3 mt-2">
+                            <div class="card-inside shadow-lg border border-2 border border-dark align-items-center">
+                                <div class="card-inside1 float-start">
+                                    <div class="font-ci1">0</div>
+                                </div>
+                                <div class="card-inside2 float-end">
+                                    <div class="font-ci2">DEKOK</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-3 mt-2">
+                            <div class="card-inside shadow-lg border border-2 border border-dark align-items-center">
+                                <div class="card-inside1 float-start">
+                                    <div class="font-ci1">0</div>
+                                </div>
+                                <div class="card-inside2 float-end">
+                                    <div class="font-ci2">GOMPAL</div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
 
