221cf8d113e204e98ee70f6381a337dd15b761b7
Leonardo Tarung <leotarung@gmail.com>
2023-02-08 13:26:27 +0700

    resume forklift update

--- app/Http/Controllers/UsableController.php
+++ app/Http/Controllers/UsableController.php
@@ -80,8 +80,7 @@ class UsableController extends Controller
         $shift = $useable->Shift();
         $date = $useable->date();
 
-
-        $sql1 = LhpSupplyRAW::groupBy(LhpMeltingRAW::raw('no_mc', 'hour(jam)'))->where([['tanggal', '=', $date], ['forklift', '=', $mesin]])->selectRaw("tanggal, jam, furnace, no_mc as Mesin_Casting, jumlah_tapping, COUNT(jumlah_tapping) as frekuensi, SUM(jumlah_tapping) as total_tapping")->get();
+        $sql1 = LhpSupplyRAW::groupBy(LhpSupplyRAW::raw('no_mc'))->groupBy(LhpSupplyRAW::raw('hour(jam)'))->where([['tanggal', '=', $date], ['forklift', '=', $mesin]])->selectRaw("tanggal, jam, furnace, no_mc as Mesin_Casting, jumlah_tapping, COUNT(jumlah_tapping) as frekuensi, SUM(jumlah_tapping) as total_tapping")->get  ();
         // dd($sql1);
         return view('lhp.resume-forklift', compact('sql1', 'mesin', 'id'));
     }
--- resources/views/settings/mainSettings.blade.php
+++ resources/views/settings/mainSettings.blade.php
@@ -48,7 +48,7 @@
     {{-- MODAL --}}
     <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
         aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog modal-xl">
+        <div class="modal-dialog modal-fullscreen">
             <div class="modal-content">
                 <div class="modal-header">
                     <h1 class="modal-title fs-5" id="staticBackdropLabel"></h1>
